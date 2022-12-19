c514ae5ad4e22a95943506b9963f324534dbee6f
iibme <120074173+iibme@users.noreply.github.com>
2022-12-19 10:30:42 +0700

    Add files via upload

---
+++ Ex4_Witoon_R.py
@@ -0,0 +1,10 @@
+Fe = 85
+Gb = 74
+ICS = 79
+CP = 88
+print("--- Your Score ---")
+print("Foundation English :", Fe)
+print("General Business :", Gb)
+print("Introduction to Computer systems :", ICS)
+print("Computer Programing :", CP)
+
