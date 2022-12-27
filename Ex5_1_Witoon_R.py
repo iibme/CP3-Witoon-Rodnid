ad9720d70b63ddd235ea7262c086b56aa5642b36
iibme <120074173+iibme@users.noreply.github.com>
2022-12-27 09:52:38 +0700

    Add files via upload

---
+++ Ex5_1_Witoon_R.py
@@ -0,0 +1,6 @@
+x = input()
+y = input()
+print(x,"+",y,"=", int(x)+int(y))
+print(x,"-",y,"=", int(x)-int(y))
+print(x,"*",y,"=", int(x)*int(y))
+print(x,"/",y,"=", int(x)/int(y))
---
+++ Ex5_2_Witoon_R.py
@@ -0,0 +1,4 @@
+S = int(input("distane : "))
+T = int(input("time : "))
+print("Velocity : ",S/T,"km/h")
+
